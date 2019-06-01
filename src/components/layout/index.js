d9f30234d182fdfe910eaedcf3e13da913c60dca
Billy Bunn <williammbunn@gmail.com>
2019-06-01 12:38:28 -0700

    adds starter markup

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -2,8 +2,38 @@ import React from "react"
 
 export default ({ children }) => (
   <div>
-    <header>Header</header>
-    {children}
-    <footer>Footer</footer>
+    <header>
+      <nav>
+        <ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Projects</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+    <aside>
+      <ul>
+        <li>
+          <a href="#">GitHub</a>
+        </li>
+        <li>
+          <a href="#">LinkedIn</a>
+        </li>
+        <li>
+          <a href="#">Twitter</a>
+        </li>
+      </ul>
+    </aside>
+    <main>{children}</main>
+    <footer>Copyright © 2019 Billy Bunn</footer>
   </div>
 )
