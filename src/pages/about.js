89817e96df4ac53c49fc0e079b38a8d0500ecf62
Billy Bunn <williammbunn@gmail.com>
2019-06-03 07:03:20 -0700

    starts styling for about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,28 +1,59 @@
 import React from "react"
-// import styled from "styled-components"
+import styled from "styled-components"
 import Layout from "../components/layout"
+
+const Container = styled.div`
+  ${"" /* align-items: center; */}
+  display: flex;
+  flex-flow: column nowrap;
+  height: 100%;
+  padding: 0 1rem;
+  ${"" /* justify-content: center; */}
+  > h2 {
+    font-size: 2.5rem;
+    font-weight: 600;
+    margin: 0 auto;
+    text-transform: uppercase;
+  }
+  > h4 {
+    
+  }
+  > p {
+    font-size: 1rem;
+    line-height: calc(1rem * 1.5);
+    margin: 0 0 1rem 0;
+  }
+`
 
 export default () => (
   <Layout>
-    <h2>About</h2>
-    <div>
-      <img src="https://via.placeholder.com/150" alt="headshot" />
+    <Container>
+      <h2>About</h2>
+      {/* <img src="https://via.placeholder.com/150" alt="headshot" /> */}
+      <h4>Hi there, I'm Billy.</h4>
+
       <p>
-        Right out of high school, I moved from Northern California to Seattle. I
-        spent several years working in online retail, copywriting, and
-        bookkeeping while getting my BA in accounting from the University of
-        Washington. After graduating, I went straight to work for an
-        international accounting firm as a financial statement auditor. There, I
-        worked on client teams to serve middle-market companies in a number of
-        industries. In late 2018, I left the finance world to pursue my passion
-        and study coding full-time.
+        I'm a full-stack web developer specializing in modern JavaScript tools
+        and libraries. I've built things with React, Node, Express, MongoDB, and
+        more.
       </p>
+
+      <h4>My path to software development was unconventional.</h4>
+
       <p>
-        Currently looking to join a team where I can stretch myself, learn from
-        veteran developers, and have my code held to a high standard. I'm
-        experienced with the MERN stack (MongoDB, Express, React, Node.js), but
-        always hungry to learn more tools and languages.
+        I landed a great job out of college at an international accounting firm
+        as a financial statement auditor.
       </p>
-    </div>
+
+      <p>I worked with clients in a number of industries</p>
+
+      <p>I'm a software developer with a background in finance.</p>
+      <p>
+        I completed a B.A. in Accounting from the University of Washington and
+        went straight to work for an international accounting firm as a
+        financial statement auditor.
+      </p>
+      <p />
+    </Container>
   </Layout>
 )
