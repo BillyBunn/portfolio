1d6caac262ebc6e914ae7c229737663f1929ce19
Billy Bunn <williammbunn@gmail.com>
2019-06-19 15:51:06 -0700

    Updates

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,4 +0,0 @@
-module.exports = [{
-      plugin: require('/Users/billybunn/Code/projects/billybunn/node_modules/gatsby-remark-images/gatsby-browser.js'),
-      options: {"plugins":[],"maxWidth":590},
-    }]
