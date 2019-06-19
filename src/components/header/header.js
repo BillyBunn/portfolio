8cb94e73c95d99e84f2488aecd498675c888ccf6
Billy Bunn <williammbunn@gmail.com>
2019-06-19 16:13:36 -0700

    uses custom window size hook for header rendering

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,29 +1,14 @@
 import React, { useState, useEffect } from "react"
+import useWindowSize from "../../hooks/useWindowSize"
 
 import DesktopHeader from "./desktop-header"
 import MobileHeader from "./mobile-header"
 
 export default ({ breakPoint }) => {
   const routes = ["about", "projects", "contact"]
-  const [width, setWidth] = useState(
-    typeof window !== "undefined" ? window.innerWidth : 1025
-  )
-
-  useEffect(() => {
-    const handleResize = () => {
-      setWidth(typeof window !== "undefined" ? window.innerWidth : 1025)
-    }
-
-    if (typeof window !== "undefined")
-      window.addEventListener("resize", handleResize)
-
-    return () => {
-      if (typeof window !== "undefined")
-        window.removeEventListener("resize", handleResize)
-    }
-  }, [])
-
-  return width > breakPoint ? (
+  const size = useWindowSize()
+  
+  return size.width > breakPoint ? (
     <DesktopHeader routes={routes} />
   ) : (
     <MobileHeader routes={routes} />
