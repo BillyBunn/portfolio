906016bc035a99cfb2bc7f316535f27face1e8d5
Billy Bunn <williammbunn@gmail.com>
2019-06-21 11:15:13 -0700

    header fix

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,19 +1,41 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import useWindowSize from "../../hooks/useWindowSize"
 
 import DesktopHeader from "./desktop-header"
 import MobileHeader from "./mobile-header"
 
+// export default ({ breakPoint }) => {
+//   const routes = ["about", "projects", "contact"]
+//   const size = useWindowSize()
+
+//   // console.log("SIZE WIDTH", size.width)
+//   // console.log("HELLO WORLD")
+
+//   if (size.width > breakPoint) {
+//     return <DesktopHeader routes={routes} />
+//   } else if (size.width < breakPoint) {
+//     return <MobileHeader routes={routes} />
+//   } else {
+//     return <DesktopHeader routes={routes} />
+//   }
+// }
+
 export default ({ breakPoint }) => {
   const routes = ["about", "projects", "contact"]
   const size = useWindowSize()
+  const [isMobile, setIsMobile] = useState(true)
 
-  // console.log("SIZE WIDTH", size.width)
-  // console.log("HELLO WORLD")
+  useEffect(() => {
+    if (size.width > breakPoint) {
+      // console.log("size.width < breakpoint", size.width)
+      setIsMobile(false)
+    } else {
+      // console.log("size.width < breakpoint", size.width)
+      setIsMobile(true)
+    }
+  }, [size])
 
-  if (size.width > breakPoint) {
-    return <DesktopHeader routes={routes} />
-  } else if (size.width < breakPoint) {
+  if (isMobile) {
     return <MobileHeader routes={routes} />
   } else {
     return <DesktopHeader routes={routes} />
