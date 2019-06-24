9cbfb0e7074470408ef5d0274246449ddae8ac6c
Billy Bunn <williammbunn@gmail.com>
2019-06-24 15:38:00 -0700

    updates home page styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,16 +3,23 @@ import styled from "styled-components"
 import Layout from "components/layout"
 
 const Splash = styled.div`
-  align-items: center;
-  display: flex;
-  flex-flow: column nowrap;
-
-  ${"" /* width: 100vw; */}
+  margin: 25vh 0;
   > * {
     text-align: center;
   }
   > h1 {
+    animation: color-change 10s infinite;
     font-size: 500%;
+    font-weight: 600;
+    line-height: 1;
+
+    @keyframes color-change {
+      0%   {color: var(--primary);}
+      10%  {color: var(--primary-light);}
+      50%  {color: var(--accent);}
+      90%  {color: var(--primary-light);}
+      100%   {color: var(--primary);}
+    }
   }
   > h2 {
     font-size: 130%;
