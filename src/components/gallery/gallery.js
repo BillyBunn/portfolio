095f51b8fecbd5495d4545136f7361ef974006ed
Billy Bunn <williammbunn@gmail.com>
2019-08-08 12:28:25 -0700

    updates gallery to have clickable cards, updates styles with pose animations

--- src/components/gallery/gallery.js
+++ src/components/gallery/gallery.js
@@ -3,16 +3,17 @@ import PropTypes from 'prop-types';
 import Item from 'components/gallery/item';
 import { Container } from './gallery.css';
 
-const Gallery = ({ items }) => (
+const Gallery = ({ items, clickable }) => (
   <Container>
     {items.map((item, i) => (
-      <Item {...item} key={i} />
+      <Item {...item} key={i} clickable={clickable} />
     ))}
   </Container>
 );
 
 Gallery.propTypes = {
   items: PropTypes.arrayOf(PropTypes.object).isRequired,
+  clickable: PropTypes.bool,
 };
 
 export default Gallery;
